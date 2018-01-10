ab1befc36306b9769927dcf07b2985d48f995411
Unknown <finn_harms@web.de>
2018-01-10 12:00:18 +0100

    Hinzufügen einer World
    Eine Klasse World wurde hinzugefügt, in welcher die Welt gemacht werden soll.

--- Jump-n-Run/src/de/L118/game/Player.java
+++ Jump-n-Run/src/de/L118/game/Player.java
@@ -1,8 +1,19 @@
 package de.L118.game;
 
 public class Player {
+	World w;
 	
-	public Player() {
-		// TODO Auto-generated constructor stub
+	public Player(World w) {
+		this.w = w;
 	}
+	
+	public void jump() {
+		
+	}
+	
+	public void update() {
+		
+	}
+	
+	
 }
---
+++ Jump-n-Run/src/de/L118/game/World.java
@@ -0,0 +1,34 @@
+package de.L118.game;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class World {
+	
+	List<Blocks> blocks;
+	
+	public World() {
+		blocks = new LinkedList<Blocks>();
+	}
+	
+	boolean isObjekt(int x, int y) {
+		//TODO
+		return false;
+	}
+	
+	void moveLeft(int distance) {
+		//TODO
+	}
+	
+	void moveRight(int distance) {
+		//TODO
+	}
+	
+	void loadWorld() {
+		//TODO
+	}
+	
+	void update() {
+		//TODO
+	}
+}
